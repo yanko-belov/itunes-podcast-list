f4dd8108cd739617f2cedd4c2e49b8040f3649fb
Yanko Belov <belov.yanko@gmail.com>
2023-05-02 19:40:37 +0300

    feat: Create episodes table

---
+++ components/episode-table.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+import { Episode } from "@/types";
+import Link from "next/link";
+
+interface EpisodeTableProps {
+  episodes: Episode[]
+}
+
+const EpisodeTable: FC<EpisodeTableProps> = ({episodes}) => {
+  const formatDate = (isoDate:string) => {
+    const date = new Date(isoDate);
+    return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+  }
+  const formatTime = (trackTimeMillis:number) => {
+    return new Date(trackTimeMillis).toISOString().slice(11,19);
+  }
+
+  return (
+    <table className="w-full text-sm text-left text-gray-500">
+      <thead className="text-xs text-gray-700 uppercase">
+      <tr>
+        <th scope="col" className="px-6 py-3">
+          Title
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Date
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Duration
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      {episodes.map((episode, index) => (
+        <tr key={episode.episodeGuid} className={`border-b ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>
+          <th scope="row" className="px-6 py-4 font-medium text-gray-900">
+            <Link className='text-blue-400 hover:text-blue-500' href={`/podcast/${123}/episode/${123}`}>
+              {episode.trackName}
+            </Link>
+          </th>
+          <td className="px-6 py-4">
+            {formatDate(episode.releaseDate)}
+          </td>
+          <td className="px-6 py-4">
+            {formatTime(episode.trackTimeMillis)}
+          </td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default EpisodeTable;
