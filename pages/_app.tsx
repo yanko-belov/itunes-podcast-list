1a1442527f9fc93e05ed567eac63f640a75a8231
Yanko Belov <belov.yanko@gmail.com>
2023-05-03 13:28:13 +0300

    fix: Remove ReactQuery devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,7 +47,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
