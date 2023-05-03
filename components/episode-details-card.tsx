7df1d1f9d35870718603b1837a99c06bd5b5f04a
Yanko Belov <belov.yanko@gmail.com>
2023-05-03 18:07:10 +0300

    feat: Show HTML if any in episode description

--- components/episode-details-card.tsx
+++ components/episode-details-card.tsx
@@ -1,14 +1,30 @@
 import { FC } from "react";
 import { Episode } from "@/types";
+import sanitizeHtml from "sanitize-html";
+
 interface EpisodeDetailsProps {
   episode: Episode;
 }
 
 const EpisodeDetailsCard: FC<EpisodeDetailsProps> = ({ episode }) => {
+  const defaultOptions = {
+    allowedTags: ["b", "i", "em", "strong", "a"],
+    allowedAttributes: {
+      a: ["href"],
+    },
+  };
+
+  const sanitize = (dirty: string) => ({
+    __html: sanitizeHtml(dirty, { ...defaultOptions }),
+  });
+
   return (
     <div className="rounded-md bg-white px-6 py-4 shadow-md">
       <h1 className="mb-4 text-xl font-bold">{episode.trackName}</h1>
-      <div className="italic">{episode.description}</div>
+      <div
+        className="italic"
+        dangerouslySetInnerHTML={sanitize(episode.description)}
+      />
       <hr className="my-6" />
       <audio controls className="w-full">
         <source
--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
 const nextConfig = {
   reactStrictMode: true,
+  distDir: isDevelopment ? ".next-dev" : ".next",
+  webpack: (config) => {
+    if (isDevelopment) {
+      config.optimization.minimize = false;
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
